6d3aac4b1d4aa2d4570c6d05bdbed55bfdf8ba2d
swarno-coder <swarnodipnag2004@gmail.com>
2023-06-30 11:29:43 +0530

    Some Updates

--- run_PersonalGPT.py
+++ run_PersonalGPT.py
@@ -1,9 +1,10 @@
-from PersonalGPT.engine import takeCommand
-from PersonalGPT.Jarvis import call
+from PersonalGPT import takeCommand
+from PersonalGPT import call
 
 while True:
-    some = takeCommand(say=None)
-    if 'jarvis' or 'hey jarvis' in some:
-        some = some.replace('hey jarvis' ,'')
-        call(some)
+    com = takeCommand(say=None)
+    print(com)
+    if 'jarvis' or 'hey jarvis' in com:
+        com = com.replace('hey jarvis' ,'')
+        call(com)
     print('/')
